9cff2ff925137215009d6a4aa3528a0e1cff624a
MileColmenares <132697364+MileColmenares@users.noreply.github.com>
2023-08-07 18:09:48 -0300

    CABECERA

---
+++ cabecera.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Centro de estudiantes</title>
+    <link rel="shortcut icon" href="./img/logoipet-favicon.jpg">
+    <!--BOOSTRAP--> 
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+   <!--CSS-->
+   <link rel="stylesheet" href="css/cabecera.css">
+   <!--JAVA-->
+       <script src="js/style1.js"></script>
+</head>
+<body>
+    <header>
+    <nav>
+    <div class="container-fluid">
+        <h1>TEXTO<img href="" src= "img/person-circle-outline.svg" height="50px" class= "user"></h1>
+    </div>
+</nav>
+
+    </header>
+ 
+</body>
+</html>
